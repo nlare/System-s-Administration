6a42fec7b3ac5b8517c1a2a4f43576580e5bc368
Nathan Lare <96361319+nlare@users.noreply.github.com>
2024-02-29 19:55:59 -0600

    Update EXOEnableArchiving.ps1

--- EXOEnableArchiving.ps1
+++ EXOEnableArchiving.ps1
@@ -1,12 +1,13 @@
 ﻿<# This script enables archiving for users who have reached > <setamount> on their mailbox, in order to clear up space and maintain retention policy.
-you simply need to set your threshold for archiving to kick off, and the name of the retention policy you want to use.#>
+you simply need to set your threshold for archiving to kick off, the name of the retention policy you want to use, and organzation domain.#>
 
-$rp = '<Insert Name of Retention Policy>'
-$mbthreshold = '<Insert threshold in which you want archiving to be enabled when reached>'
 Import-Module ExchangeOnlineManagement
+$rp = ''
+$mbthreshold = ''
+$orgDomain = ''
 
 # Connect to EXO with managed identity
-Connect-ExchangeOnline -ManagedIdentity -Organization <insert organization domain>
+Connect-ExchangeOnline -ManagedIdentity -Organization $orgDomain
 
 # Retrieve all mailboxes and gather current mailbox size
 $mbabove89 = ForEach ($mailbox in (Get-EXOMailbox)) {
